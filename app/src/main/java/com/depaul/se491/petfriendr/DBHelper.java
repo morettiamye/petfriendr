ec79e85f95491b548ab441f6d09ff62edf4fa98a
salemij93 <salemi-j@hotmail.com>
2022-03-11 20:59:53 -0600

    added code to set user info to DBHelper.java, must be added to correct class and is commented out

--- app/src/main/java/com/depaul/se491/petfriendr/DBHelper.java
+++ app/src/main/java/com/depaul/se491/petfriendr/DBHelper.java
@@ -9,36 +9,54 @@ import android.net.Uri;
 
 public class DBHelper extends AppCompatActivity {
 
-    //allows acces to user info and must be put into appropriate classes
-    private FirebaseAuth mAuth;
-    mAuth = FirebaseAuth.getInstance();
-//check to see if user is logged in already
-    public void onStart() {
-        super.onStart();
-        // Check if user is signed in (non-null) and update UI accordingly.
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        if(currentUser != null){
-            //reload(); code to update logged in user
-        }
-    }
+    //allows access to user info and must be put into appropriate classes
+//    private FirebaseAuth mAuth;
+//    mAuth = FirebaseAuth.getInstance();
+////check to see if user is logged in already
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        if(currentUser != null){
+//            //reload(); code to update logged in user
+//        }
+//    }
+//
+//
+//    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//
+//    if(user !=null)    {
+//            // Name, email address, and profile photo Url
+//            String name = user.getDisplayName();
+//            String email = user.getEmail();
+//            Uri photoUrl = user.getPhotoUrl();
+//
+//            // Check if user's email is verified
+//            boolean emailVerified = user.isEmailVerified();
+//
+//            // The user's ID, unique to the Firebase project. Do NOT use this value to
+//            // authenticate with your backend server, if you have one. Use
+//            // FirebaseUser.getIdToken() instead.
+//            String uid = user.getUid();
+//
+//        }
 
-
-    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-    if(user !=null)    {
-            // Name, email address, and profile photo Url
-            String name = user.getDisplayName();
-            String email = user.getEmail();
-            Uri photoUrl = user.getPhotoUrl();
-
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            // The user's ID, unique to the Firebase project. Do NOT use this value to
-            // authenticate with your backend server, if you have one. Use
-            // FirebaseUser.getIdToken() instead.
-            String uid = user.getUid();
-
-        }
+    // set user info code below
+//    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+//
+//    UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+//            .setDisplayName("Jane Q. User")
+//            .setPhotoUri(Uri.parse("https://example.com/jane-q-user/profile.jpg")) // change url to location of picture in bucket
+//            .build();
+//
+//user.updateProfile(profileUpdates)
+//            .addOnCompleteListener(new OnCompleteListener<Void>() {
+//        @Override
+//        public void onComplete(@NonNull Task<Void> task) {
+//            if (task.isSuccessful()) {
+//                Log.d(TAG, "User profile updated.");
+//            }
+//        }
+//    });
 
 }
