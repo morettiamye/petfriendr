b767e9b58d78a1f26cbc3b5783cc991d7ddb65c9
salemij93 <salemi-j@hotmail.com>
2022-03-17 09:08:06 -0500

    updated DisplayProfileActivity.java to create comments

--- app/src/main/java/com/depaul/se491/petfriendr/DisplayProfileActivity.java
+++ app/src/main/java/com/depaul/se491/petfriendr/DisplayProfileActivity.java
@@ -9,16 +9,33 @@
 
 package com.depaul.se491.petfriendr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.depaul.se491.petfriendr.models.Comment;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.squareup.picasso.Picasso;
 
 public class DisplayProfileActivity extends BaseActivity {
+
+    private DatabaseReference mDatabase;
+    Button submitComment;
+    EditText newCommentText;
+    FirebaseUser mUser;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,5 +53,37 @@ public class DisplayProfileActivity extends BaseActivity {
         textPet.setText(petName);
         textMessage.setText(message);
         Picasso.get().load(imageUrl).into(imagePic);
+        newCommentText = findViewById(R.id.editTextTextPersonName);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        submitComment = findViewById(R.id.submitComment);
+        submitComment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addNewComment(userName);
+            }
+        });
+    }
+
+    private void addNewComment(String userName){
+        String newComment = newCommentText.getText().toString();
+
+        mAuth  = FirebaseAuth.getInstance();
+        mUser = mAuth.getCurrentUser();
+        Comment newCommentData = new Comment(mUser,newComment,userName);
+        mDatabase.child("comments").child(mUser.getUid()).setValue(newCommentData).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                // Write was successful!
+                // ...
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Write failed
+                        // ...
+                    }
+                });
     }
 }
