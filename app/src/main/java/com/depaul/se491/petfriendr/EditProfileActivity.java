eb65ecf1bb5f948dc5511a4cb6477f3d7831ea2d
salemij93 <salemi-j@hotmail.com>
2022-03-18 00:19:23 -0500

    upload photo is working and SignUpActivity.java creates a new user in the database

--- app/src/main/java/com/depaul/se491/petfriendr/EditProfileActivity.java
+++ app/src/main/java/com/depaul/se491/petfriendr/EditProfileActivity.java
@@ -3,17 +3,31 @@ package com.depaul.se491.petfriendr;
 import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.AlertDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.widget.Button;
 import android.widget.ImageView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
 
 public class EditProfileActivity extends AppCompatActivity {
@@ -22,11 +36,22 @@ public class EditProfileActivity extends AppCompatActivity {
 
     private ImageView imageProfile;
     private Button updateProfile;
+    private Button uploadPhoto;
+    StorageReference storageRef;
+    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private DatabaseReference mDatabase;
+    private DatabaseReference mUsersRef;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        this.storageRef = storage.getReference();
 
         // Register image capture activity callback
         cameraLauncher = registerForActivityResult(
@@ -35,11 +60,14 @@ public class EditProfileActivity extends AppCompatActivity {
         galleryLauncher = registerForActivityResult(
                 new ActivityResultContracts.StartActivityForResult(),
                 this::handleGalleryResult);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         imageProfile = (ImageView) findViewById(R.id.imageProfile);
         updateProfile = (Button) findViewById(R.id.updateProfile);
+        uploadPhoto = (Button) findViewById(R.id.uploadProfilePic_button);
         imageProfile.setOnClickListener(v -> showDialogGetProfileImage());
         updateProfile.setOnClickListener(v -> showDialogSaveChanges());
+        uploadPhoto.setOnClickListener(v -> uploadNewPhoto());
     }
 
 
@@ -96,5 +124,34 @@ public class EditProfileActivity extends AppCompatActivity {
     }
 
     private void storeUserInfo() {
+    }
+
+    private void uploadNewPhoto(){
+        String uid = user.getUid();
+        StorageReference profilepicRef = storageRef.child(uid);
+        StorageReference profilepicImageRef = storageRef.child("images/"+uid);
+        imageProfile.setDrawingCacheEnabled(true);
+        imageProfile.buildDrawingCache();
+        Bitmap bitmap = ((BitmapDrawable) imageProfile.getDrawable()).getBitmap();
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+        byte[] data = baos.toByteArray();
+        UploadTask uploadTask = profilepicRef.putBytes(data);
+        uploadTask.addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+               String photo = taskSnapshot.getMetadata().getReference().getDownloadUrl().toString();
+                 mDatabase.child("users").child(user.getUid()).child("photo").setValue(photo);
+        
+            }
+        });
+
+
+
     }
 }
